7975989c99157dea1c1b5c04da9d02a6e22a5a8c
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-01-25 07:27:02 +0100

    Fix method name

--- test/common/file.storage.test.ts
+++ test/common/file.storage.test.ts
@@ -11,7 +11,7 @@ test('common:file.storage.service', async t => {
     const result = await fileStorage.uploadFile('file.storage.jpg', Buffer.from('./file.storage.jpg'));
     assert.ok(result);
 
-    const fileUrl = await fileStorage.resolveByFilename('file.storage.jpg');
+    const fileUrl = await fileStorage.resolveFileUrl('file.storage.jpg');
     assert.ok(fileUrl);
 
     await fileStorage.deleteFile('file.storage.jpg');
