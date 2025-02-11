f3a18c17f984e61dc8718c9dad477a51f78ad4b3
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-02-11 10:23:13 +0100

    Configure render.com

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { fastify } from 'fastify';
-import app from './app'; // adjust path if needed
-import { appOptions } from './app'; // adjust path if needed
+import basicAuth from '@fastify/basic-auth';
+import app from './app';
+import { appOptions } from './app';
 
 const server = fastify({
   logger: {
@@ -20,11 +21,24 @@ const server = fastify({
 server.get('/health', { logLevel: 'silent' }, () => 'ok');
 server.get('/', { logLevel: 'silent' }, () => 'ok');
 
+// Authorization
+const authenticate = { realm: 'philadelphia-backend-api' };
+const validate = async (username: string, password: string) => {
+  if (username !== process.env.API_USERNAME || password !== process.env.API_PASSWORD) {
+    throw new Error('Invalid credentials');
+  }
+};
+
 const start = async () => {
   try {
     if (!process.env.PORT) {
       throw new Error('Missing PORT environment variable');
     }
+
+    // Register auth plugin
+    await server.register(basicAuth, { validate, authenticate });
+    // Protect all routes
+    server.addHook('onRequest', server.basicAuth);
 
     await server.register(app, appOptions);
     await server.listen({
