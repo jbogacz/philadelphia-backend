c050d283c99132284e6ae45ea7d875cbc3ab4836
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-02-11 09:31:38 +0100

    Configure render.com

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,36 @@
-import { fastify } from 'fastify'
-import app from './app'  // adjust path if needed
-import { appOptions } from './app'  // adjust path if needed
+import { fastify } from 'fastify';
+import app from './app'; // adjust path if needed
+import { appOptions } from './app'; // adjust path if needed
 
 const server = fastify({
-  logger: true
-})
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+        colorize: true,
+      },
+    },
+  },
+});
 
 const start = async () => {
   try {
-
-    if (!process.env.PORT){
-      throw new Error('Missing PORT environment variable')
+    if (!process.env.PORT) {
+      throw new Error('Missing PORT environment variable');
     }
 
-    await server.register(app, appOptions)
+    await server.register(app, appOptions);
     await server.listen({
       port: parseInt(process.env.PORT),
-      host: '0.0.0.0'
-    })
-    console.log(`Server is running on port ${process.env.PORT}`)
+      host: '0.0.0.0',
+    });
+    console.log(`Server is running on port ${process.env.PORT}`);
   } catch (err) {
-    server.log.error(err)
-    process.exit(1)
+    server.log.error(err);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
