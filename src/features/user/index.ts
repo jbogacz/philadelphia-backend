13911de013db87c179eab4f1816f2ddbe588410a
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-02-17 08:40:19 +0100

    Remove 'auth' domain * use Clerk to authorize users * use 'user' domain

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -1,9 +1,7 @@
 import { FastifyPluginAsync } from 'fastify';
-import { authRoutes } from './auth.routes';
 import { userRoutes } from './user.routes';
 
 const featurePlugin: FastifyPluginAsync = async (fastify) => {
-  await fastify.register(authRoutes);
   await fastify.register(userRoutes);
 };
 
