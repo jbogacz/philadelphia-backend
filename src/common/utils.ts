c4bc0ad6d87db4cab3d0da82cafd4ea3c4e4a413
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-03-06 13:00:02 +0100

    Migrate db on prod

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,1 +1,8 @@
 export const randomId = (): string => Math.random().toString(36).substring(2, 9);
+
+export const clearEnvCache = () => {
+  Object.keys(process.env).forEach((key) => {
+    delete process.env[key];
+  });
+  require('dotenv').config();
+};
