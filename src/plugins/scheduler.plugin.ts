1358bc17d57de1ead961786dcc98c4652d78abc0
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-05-06 09:59:55 +0200

    Configure scheduler

--- src/plugins/scheduler.plugin.ts
+++ src/plugins/scheduler.plugin.ts
@@ -1,21 +1,34 @@
 import { FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin';
 import schedule from '@fastify/schedule';
-import { SimpleIntervalJob, Task } from 'toad-scheduler';
+import { CronJob, SimpleIntervalJob, Task } from 'toad-scheduler';
 import { AppOptions } from '../app.types';
+import { CampaignSchedulerService } from '../features/marketplace/campaign/campaign.scheduler.service';
 
 const scheduler: FastifyPluginAsync<AppOptions> = async (fastify, opts) => {
   const { config } = opts;
   void fastify.register(schedule);
 
   fastify.ready().then(() => {
-    const task = new Task('task', () => {
-      fastify.log.info('CronJob every 5 seconds');
-    });
-    const job = new SimpleIntervalJob({ seconds: 1 }, task);
-
+    // Scheduler for campaign activation and completion
     if (config.scheduler.campaign.enabled) {
-      fastify.scheduler.addSimpleIntervalJob(job);
+      if (config.scheduler.campaign.cron === '') {
+        fastify.log.error('Campaign scheduler cron expression is empty');
+        return;
+      }
+
+      const cronSchedule = {
+        cronExpression: config.scheduler.campaign.cron,
+      };
+
+      const task = new Task('processCampaignsTask', async () => {
+        const campaignService = new CampaignSchedulerService(fastify.repository.campaign);
+        await campaignService.activateCampaigns();
+        await campaignService.completeCampaigns();
+      });
+      const job = new CronJob(cronSchedule, task);
+
+      fastify.scheduler.addCronJob(job);
     }
   });
 };
